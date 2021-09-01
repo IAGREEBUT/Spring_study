5db9a338d00f23b6c6b37082f9316124355e797d
YujeongLEE <dldbwjd24@ewhain.net>
2021-09-01 23:09:14 +0900

    8.API
    _ResponseBody

--- hellospring/src/main/java/hello/hellospring/controller/HelloController.java
+++ hellospring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -21,9 +22,14 @@ public class HelloController {
     public String helloMvc(@RequestParam("name") String name, Model model){
         //parameter를 요청 , 정보를 담아서 view로델 보낼 모델
 
-
         model.addAttribute("name",name);// 받은 파라미터를 이용해서 name키의 값으로 지정
         return "hello-template"; //hello-template.html로
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody // http의 body부분에 이 데이터를 직접 넣어 주겠다는 의미
+    public String helloString(@RequestParam("name") String name){
+        return "hello" + name; //viewResolver가 아니고 data가 그대로 전달됨 
     }
 
 
