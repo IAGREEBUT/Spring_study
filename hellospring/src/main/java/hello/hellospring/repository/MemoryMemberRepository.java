de2052149460c3b240059814c24485c1758ed870
YujeongLEE <dldbwjd24@ewhain.net>
2021-09-07 16:12:16 +0900

    10. 회원 도메인과 레포지토리 만들기
    Interface를 기반으로 MemoryMemberRepository 클래스 구현하기

---
+++ hellospring/src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -0,0 +1,34 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+
+import java.lang.reflect.Array;
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberRepository{
+
+    private static Map<Long, Member> store = new HashMap<>(); // 데이터를 저장할 Map 자료형
+    private static long sequence = 0L; // ID용
+
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence); //ID setting
+        store.put(member.getId(),member); // Map에 저장
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(store.get(id)); //결과가 없다면
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values().stream().filter(member -> member.getName().equals(name)).findAny();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+}
