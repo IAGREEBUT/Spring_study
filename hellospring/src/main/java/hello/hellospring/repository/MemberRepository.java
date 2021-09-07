6bc8d2112e67e49c2aedb2ccf8b508578d48a045
YujeongLEE <dldbwjd24@ewhain.net>
2021-09-07 16:11:50 +0900

    10. 회원 도메인과 레포지토리 만들기
    회원 레포지토리 패키지와 MemberRepository Interface추가

---
+++ hellospring/src/main/java/hello/hellospring/repository/MemberRepository.java
@@ -0,0 +1,14 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+
+    Member save(Member member); //회원 등록
+    Optional<Member> findById(Long id); // 회원 조회 - ID
+    Optional<Member> findByName(String name); // 회원 조회 - Name
+    List<Member> findAll();
+}
