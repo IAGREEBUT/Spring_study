65cc46b241c2f6ca8a20f9339fc804db733fe0fa
YujeongLEE <dldbwjd24@ewhain.net>
2021-09-07 20:08:27 +0900

    11. 회원 리포지토리 테스트 케이스 작성
    여러개 메소드를 한번에 테스트

--- hellospring/src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
+++ hellospring/src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
@@ -1,2 +1,72 @@
-package hello.hellospring.repository;public class MemoryMemberRepositoryTest {
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class MemoryMemberRepositoryTest {
+
+    MemberRepository repository =  new MemoryMemberRepository();
+
+    @Test
+    public void save(){
+        Member member = new Member(); //맴버 객체 생성
+        member.setName("yujeongLEE");// name에 값 setting
+
+        repository.save(member); //save method를 실행 -> store에 방금 생성한 member저장
+
+
+        Member result = repository.findById(member.getId()).get(); // 방금 생성한 member의 ID를 이용해 findById로 맴버가 잘 찾아지는지 확인
+
+//        1. 콘솔창에 출력해보기
+//        System.out.println("result = " + (result == member)); -> 내가 생성한 member와 DB에서 가져온 결과가 같으면 "result = true"가 콘솔창에 출력됨
+
+//        2. Assertions 이용 : 둘이 같은지 확인가능 매개변수(expected, actual)
+//        Assertions.assertEquals(member, result); // 같은 경우테스트용
+//        Assertions.assertEquals(member, null); //다른 경우테스트용
+
+//        3. Assertions 이용(assertj)
+        assertThat(member).isEqualTo(result);
+
+    }
+
+
+
+    @Test
+    public void findByName() {
+        Member member1 = new Member();
+        member1.setName("yujeongLEE");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("hyunjiLIM");
+        repository.save(member2);
+
+        Member result = repository.findByName("yujeongLEE").get();
+
+        assertThat(result).isEqualTo(member1);//member1을 넣으면 성공 / member2를 넣으면 오류 여야함
+
+    }
+
+    @Test
+    public void findAll(){
+
+        Member member1 = new Member();
+        member1.setName("yujeongLEE");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("hyunjiLIM");
+        repository.save(member2);
+
+        List<Member> result = repository.findAll();
+
+        assertThat(result.size()).isEqualTo(2); //모든 것을 찾아내는 메소드의 사이즈가 2이면 성공해야함
+
+    }
+
 }
