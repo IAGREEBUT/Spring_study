33c1fca6f905abcb582cd4357b813142d2ef68d4
YujeongLEE <dldbwjd24@ewhain.net>
2021-09-14 18:06:02 +0900

    23. JPA
    SpringConfig를 이용해 JPARepository로 변경

--- hellospring/src/main/java/hello/hellospring/SpringConfig.java
+++ hellospring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,25 +1,31 @@
 package hello.hellospring;
 
-import hello.hellospring.repository.JdbcMemberRepository;
-import hello.hellospring.repository.JdbcTemplateMemberRepository;
-import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
+import hello.hellospring.repository.*;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.persistence.EntityManager;
 import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
 
 
-    private DataSource dataSource; //jdbc레포에 필요한 매개변수
+//    private DataSource dataSource; //jdbc레포에 필요한 매개변수
+//
+//    @Autowired
+//    public SpringConfig(DataSource dataSource) {
+//        this.dataSource = dataSource;
+//    }
+
+    //jpa는 entityManager를 사용!
+    private EntityManager em;
 
     @Autowired
-    public SpringConfig(DataSource dataSource) {
-        this.dataSource = dataSource;
+    public SpringConfig(EntityManager em){
+        this.em = em;
     }
 
 
@@ -30,7 +36,9 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository(){
-        return new JdbcTemplateMemberRepository(dataSource);
+
+        return new JpaMemberRepository(em);
+//        return new JdbcTemplateMemberRepository(dataSource);
 //        return new MemoryMemberRepository();
 //        return new JdbcMemberRepository(dataSource);
     }
