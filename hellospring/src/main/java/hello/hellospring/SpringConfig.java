4ecebfe9113549c25771c741c436144bf17249f2
YujeongLEE <dldbwjd24@ewhain.net>
2021-09-14 20:31:09 +0900

    26. AOP적용
    AOP를 bean으로 등록하기

--- hellospring/src/main/java/hello/hellospring/SpringConfig.java
+++ hellospring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,5 +1,6 @@
 package hello.hellospring;
 
+import hello.hellospring.aop.TimeTraceAop;
 import hello.hellospring.repository.*;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -42,6 +43,11 @@ public class SpringConfig {
 
         return new MemberService(memberRepository);//의존성 등록
     }
+
+//    @Bean
+//    public TimeTraceAop timeTraceAop(){
+//        return new TimeTraceAop();
+//    }
 
 //    @Bean
 //    public MemberRepository memberRepository(){
