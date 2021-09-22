4dd7cbd4c510ff6a42baf8b6d24b2626ce929224
YujeongLEE <dldbwjd24@ewhain.net>
2021-09-22 16:39:57 +0900

    (Basic) 4. 회원 도메인 개발
    회원 레포지토리 인터페이스(역할)

--- core/src/main/java/hello/core/member/MemberRepository.java
+++ core/src/main/java/hello/core/member/MemberRepository.java
@@ -2,9 +2,11 @@ package hello.core.member;
 
 public interface MemberRepository {
 
-
+    //회원을 db에 저장하는 기능
     void save(Member member);
 
+
+    //id를 이용해 회원을 찾는 기능
     Member findById(Long memberId);
 
 }
