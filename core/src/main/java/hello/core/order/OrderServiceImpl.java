4f04c36b6612fcaeb8d222e78735d504dd604ee6
YujeongLEE <dldbwjd24@ewhain.net>
2021-09-22 19:38:08 +0900

    (Basic) 6. 주문과 할인 도메인 개발
    OrderServiceImpl : 주문 서비스 구현체

---
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -0,0 +1,32 @@
+package hello.core.order;
+
+import hello.core.discount.DisountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+
+public class OrderServiceImpl implements OrderService{
+
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final DisountPolicy disountPolicy = new FixDiscountPolicy();
+
+
+
+    @Override
+    public Order creatOrder(Long memberId, String itemName, int itemPrice) { //1. 주문 생성
+
+        // 2. 회원 조회
+        Member member = memberRepository.findById(memberId);
+
+
+        // 3. 할인 적용
+        //할인에 대한 내용은 Order에서 신경쓰지 않고 결과만 사용한, discountPolicy에게 일임함 -> 잘 된 설계 : 단일 체계의 원칙이 잘 지켜짐
+        // 할인에 대한 변경이 들어올 때 OrderService는 한줄도 변화하지 않는 상태임
+        int discountPrice = disountPolicy.discount(member, itemPrice); //최종적으로 할인된 가격
+
+        // 4. 주문 결과 반환
+        return new Order(memberId, itemName, itemPrice, discountPrice); // 주문 생성하여 반환됨
+    }
+}
