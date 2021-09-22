4990bdef035cdbfa345b9fb79902bb6a21153701
YujeongLEE <dldbwjd24@ewhain.net>
2021-09-22 19:50:09 +0900

    (Basic) 7. 주문과 할인 도메인 실행과 테스트
    스프링의 도움을 받은 테스트(유닛 테스트)

--- core/src/test/java/hello/core/order/OrderServiceTest.java
+++ core/src/test/java/hello/core/order/OrderServiceTest.java
@@ -1,2 +1,40 @@
-package hello.core.order;public class OrderServiceTest {
+package hello.core.order;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class OrderServiceTest {
+
+
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService =  new OrderServiceImpl();
+
+
+    @Test
+    void createOrder() {
+
+        Long memberId = 1L; // long type은 NULL불가 wrapper type인 Long사용
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+
+        //가진 정보를 가지고 주문
+        Order order = orderService.creatOrder(memberId, "itemA",10000);
+
+
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+
+
+    }
+
+
+
+
+
+
+
 }
