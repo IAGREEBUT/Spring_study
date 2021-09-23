b3a05ea98e7c9cc58798109d7481cda2892d0511
YujeongLEE <dldbwjd24@ewhain.net>
2021-09-24 00:44:20 +0900

    (Basic) 11. AppConfig 리팩터링
    AppConfig 리팩터링

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,9 @@
 package hello.core;
 
+import hello.core.discount.DisountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -16,15 +19,23 @@ public class AppConfig { //관심사의 분리
 
 
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
         // MemberServiceImpl을 생성자를 이용해서 생성함 -> 생성시 매개변수로 구현체를 넣어줌
         // MemberServiceImpl자체에서 MemberRepository구현체를 선택하지 않고, 여기서 넣어주기 때문에 DIP/OCP를 지켰다
     }
 
-    public OrderService orderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();//나중에 구현체를 변경할 때에도 쉽게 변경하기 쉬움
     }
 
+    public OrderService orderService(){
+        return new OrderServiceImpl(memberRepository(), disountPolicy());
+    }
+
+    public DisountPolicy disountPolicy(){
+        return new RateDiscountPolicy();
+//        return new FixDiscountPolicy();
+    }
 
 
 
