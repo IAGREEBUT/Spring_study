c624be6a1fa96f9204b445e870e4d814ac5848da
YujeongLEE <dldbwjd24@ewhain.net>
2021-09-24 00:02:24 +0900

    (Basic) 10. 관심사의 분리
    스프링 테스트 라이브러리 - appConfig를 이용하도록 수정

--- core/src/test/java/hello/core/member/MemberServiceTest.java
+++ core/src/test/java/hello/core/member/MemberServiceTest.java
@@ -1,11 +1,21 @@
 package hello.core.member;
 
+import hello.core.AppConfig;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class MemberServiceTest {
 
-    MemberService memberService = new MemberServiceImpl(); // 인터페이스 = 구현체
+    MemberService memberService;
+
+    @BeforeEach
+    public void beforeEach(){ // 코드 실행전에 무조건 실행되는 코드
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+    }
+
+//    MemberService memberService = new MemberServiceImpl(); // 인터페이스 = 구현체
 
     @Test
     void join(){
--- core/src/test/java/hello/core/order/OrderServiceTest.java
+++ core/src/test/java/hello/core/order/OrderServiceTest.java
@@ -1,17 +1,30 @@
 package hello.core.order;
 
+import hello.core.AppConfig;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class OrderServiceTest {
 
 
-    MemberService memberService = new MemberServiceImpl();
-    OrderService orderService =  new OrderServiceImpl();
+    MemberService memberService;
+    OrderService orderService;
+
+    @BeforeEach
+    public void beforeEach(){ // 코드 실행전에 무조건 실행되는 코드
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+        orderService = appConfig.orderService();
+    }
+
+
+//    MemberService memberService = new MemberServiceImpl();
+//    OrderService orderService =  new OrderServiceImpl();
 
 
     @Test
@@ -30,10 +43,6 @@ public class OrderServiceTest {
 
 
     }
-
-
-
-
 
 
 
