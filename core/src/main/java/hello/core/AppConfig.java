a7929be3caffc4fcac43d360a8b4f8d13e3e8106
YujeongLEE <dldbwjd24@ewhain.net>
2021-09-24 12:03:54 +0900

    (Basic) 16. 스프링으로 전환하기
    AppConfig : @Bean, @Configuration을 이용한 스프링 컨테이너 등록

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -9,7 +9,10 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration // Spring에서 제공하는 DI 컨테이너
 public class AppConfig { //관심사의 분리
 
     //config를 통해서 memberService / OrderService(인터페이스) 를 조회시 -> 조회하는 애들은 인터페이스와만 의존관계를 가진다! (구현체를 조회한게 아니라 인터페이스를 조회함)
@@ -17,21 +20,24 @@ public class AppConfig { //관심사의 분리
     // memberSerivceImpl / OrderServiceImpl(구현체)를 반환한다.
 
 
-
-    public MemberService memberService(){
-        return new MemberServiceImpl(memberRepository());
+    @Bean
+    public MemberService memberService(){ // 메소드 명 : 스프링 빈의 이름 (key)
+        return new MemberServiceImpl(memberRepository()); // 리턴 값 : 스프링 빈의 value
         // MemberServiceImpl을 생성자를 이용해서 생성함 -> 생성시 매개변수로 구현체를 넣어줌
         // MemberServiceImpl자체에서 MemberRepository구현체를 선택하지 않고, 여기서 넣어주기 때문에 DIP/OCP를 지켰다
     }
 
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();//나중에 구현체를 변경할 때에도 쉽게 변경하기 쉬움
     }
 
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(memberRepository(), disountPolicy());
     }
 
+    @Bean
     public DisountPolicy disountPolicy(){
         return new RateDiscountPolicy();
 //        return new FixDiscountPolicy();
