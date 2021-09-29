c2444d55ea080f446d056bbc9432148aa6ebd828
YujeongLEE <dldbwjd24@ewhain.net>
2021-09-30 01:17:58 +0900

    (Basic) 21. 스프링 빈 조회 - 상속 관계

---
+++ core/src/test/java/hello/core/beanfind/ApplicationContextExtendsFindTest.java
@@ -0,0 +1,94 @@
+package hello.core.beanfind;
+
+import hello.core.AppConfig;
+import hello.core.discount.DisountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoUniqueBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class ApplicationContextExtendsFindTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
+
+
+    @Test
+    @DisplayName("부모 타입으로 조회시, 자식이 둘 이상 있으면, 중복 오류가 발생한다.")
+    void findBeanByParentTypeDuplicate(){
+//        DisountPolicy bean = ac.getBean(DisountPolicy.class);
+
+        assertThrows(NoUniqueBeanDefinitionException.class, ()-> ac.getBean(DisountPolicy.class));
+    }
+
+
+    @Test
+    @DisplayName("부모 타입으로 조회시, 자식이 둘 이상 있으면, 빈 이름을 지정하면 된다.")
+    void findBeanByParentTypeBeanName(){
+        DisountPolicy rateDiscountPolicy = ac.getBean("rateDiscountPolicy", DisountPolicy.class); //한개만 조회 가능
+        assertThat(rateDiscountPolicy).isInstanceOf(RateDiscountPolicy.class);
+    }
+
+    @Test
+    @DisplayName("(구체적인) 특정 하위 타입으로 조회") //비추천
+    void findBeanBySubType(){
+        RateDiscountPolicy bean = ac.getBean(RateDiscountPolicy.class); //DiscountPolicy가 아닌 구체적인 RateDiscountPolicy로 조회
+        assertThat(bean).isInstanceOf(RateDiscountPolicy.class);
+    }
+
+
+    @Test
+    @DisplayName("부모 타입으로 모두 조회하기")
+    void findAllBeanParentType(){
+        Map<String, DisountPolicy> beansOfType = ac.getBeansOfType(DisountPolicy.class);
+        assertThat(beansOfType.size()).isEqualTo(2);
+
+        //확인용도로 출력(실제 테스트에서는 출력문 사용안함)
+        for (String key : beansOfType.keySet()) {
+            System.out.println("key = " + key + " value = "+ beansOfType.get(key));
+        }
+    }
+
+    @Test
+    @DisplayName("Object 타입으로 모두 조회하기") // 스프링이 사용하는 모든 빈을 출력함 (자바객체는 모두 Object가 부모 타입이기때문)
+    void findAllBeanObjectType(){
+        Map<String, Object> beansOfType = ac.getBeansOfType(Object.class);
+
+
+        //확인용도로 출력(실제 테스트에서는 출력문 사용안함)
+        for (String key : beansOfType.keySet()) {
+            System.out.println("key = " + key + " value = "+ beansOfType.get(key));
+        }
+    }
+
+    @Configuration
+    static class TestConfig{
+
+        @Bean
+        public DisountPolicy rateDiscountPolicy(){
+            return new RateDiscountPolicy();
+        }
+
+        @Bean
+        public DisountPolicy fixDiscountPolicy(){
+            return new FixDiscountPolicy();
+        }
+
+
+
+    }
+
+
+
+
+
+
+}
