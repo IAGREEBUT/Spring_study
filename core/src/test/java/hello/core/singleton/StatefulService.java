c13271c169b970bfc7af920d5becb322316201be
YujeongLEE <dldbwjd24@ewhain.net>
2021-09-30 18:51:59 +0900

    (Basic) 28. 싱글톤 방식의 주의점
    stateless(지역변수)로 문제 해결

--- core/src/test/java/hello/core/singleton/StatefulService.java
+++ core/src/test/java/hello/core/singleton/StatefulService.java
@@ -3,19 +3,20 @@ package hello.core.singleton;
 public class StatefulService {
 
 
-    private int price; //상태를 유지하는 필드
+//    private int price; //상태를 유지하는 필드
 
-    public void order(String name, int price){
+    public int order(String name, int price){
 
         System.out.println("name = " + name + " price = "+price);
-        this.price = price;
 
+//        this.price = price; //여기가 문제! 공유필드가 변경됨
+        return price; //지역변수로 넘겨버리기
 
     }
 
-    public int getPrice(){
-        return price;
-    }
+//    public int getPrice(){
+//        return price;
+//    }
 
 
 }
