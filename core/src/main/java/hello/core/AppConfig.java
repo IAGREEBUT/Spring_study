809703db3881c1fd2602eec00a9f1dbfe2ad1e67
YujeongLEE <dldbwjd24@ewhain.net>
2021-09-30 19:24:32 +0900

    (Basic) 29. @Configuration과 싱글톤
    memberRepository가 몇번이나 실행되는지 확인

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -22,6 +22,7 @@ public class AppConfig { //관심사의 분리
 
     @Bean
     public MemberService memberService(){ // 메소드 명 : 스프링 빈의 이름 (key)
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository()); // 리턴 값 : 스프링 빈의 value
         // MemberServiceImpl을 생성자를 이용해서 생성함 -> 생성시 매개변수로 구현체를 넣어줌
         // MemberServiceImpl자체에서 MemberRepository구현체를 선택하지 않고, 여기서 넣어주기 때문에 DIP/OCP를 지켰다
@@ -29,11 +30,15 @@ public class AppConfig { //관심사의 분리
 
     @Bean
     public MemberRepository memberRepository() {
+
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();//나중에 구현체를 변경할 때에도 쉽게 변경하기 쉬움
     }
 
     @Bean
     public OrderService orderService(){
+
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), disountPolicy());
     }
 
