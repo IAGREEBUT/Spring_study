ea86d15bc3f6896908eac0167c8e43a2c02fa0b7
YujeongLEE <dldbwjd24@ewhain.net>
2021-10-06 23:37:07 +0900

    (Basic) 44. 빈 생명주기 콜백 시작
    빈 생명주기 콜백 시작 테스트예제

---
+++ core/src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -0,0 +1,31 @@
+package hello.core.lifecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class BeanLifeCycleTest {
+
+    @Test
+    public void lifeCycleTest(){
+        ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
+        NetworkClient client = ac.getBean(NetworkClient.class);
+
+        ac.close(); //ApplicationContext에는 close() method가 없음 -> Configurable Application Context사용
+
+    }
+
+    @Configuration
+    static class LifeCycleConfig{
+
+        @Bean
+        public NetworkClient networkClient(){
+            NetworkClient networkClient = new NetworkClient(); //생성자에서 url을 주입해주지 않음 -> 출력 시 null
+            networkClient.setUrl("http://hello-spring.dev");// 나중에 setting해주어야 하는 상황이 많이 발생
+            return networkClient;
+        }
+
+    }
+}
