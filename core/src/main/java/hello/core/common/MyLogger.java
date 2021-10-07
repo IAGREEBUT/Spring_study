00bc5b5ee0566281aed2697f4c7e1a26866d9a36
YujeongLEE <dldbwjd24@ewhain.net>
2021-10-07 20:15:42 +0900

    (Basic) 53. request 스코프 예제 만들기
    MyLogger: request scope를 이용한 로거 출력클래스

---
+++ core/src/main/java/hello/core/common/MyLogger.java
@@ -0,0 +1,41 @@
+package hello.core.common;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import java.util.UUID;
+
+
+@Component
+@Scope(value="request")//HTTP 요청당 1개씩 생성, HTTP요청이 끝나면 소멸
+public class MyLogger {
+
+    private String uuid;
+    private String requestURL; //빈이 생성되는 순간 값이 들어오지 않고 외부에서 값을 받아 setter를 이용하여 입력받음
+
+    public void setRequestURL(String requestURL){
+        this.requestURL=requestURL;
+    }
+
+
+    public void log(String message){
+        System.out.println("[ "+uuid+" ]" + "[ " + requestURL + " ] "+message);
+    }
+
+    @PostConstruct
+    public void init(){ //최초로 스프링에 이 클래스가 요청될 시 호출됨
+        uuid = UUID.randomUUID().toString(); //절대로 겹치지 않은 번호
+        System.out.println("[ "+uuid+" ] request scope bean create: "+this);//주소 출력
+    }
+
+    @PreDestroy
+    public void close(){//요청에 대한 답변이 나가면 호출됨
+        System.out.println("[ "+uuid+" ] request scope bean close: "+this);//주소 출력
+
+    }
+
+
+
+}
